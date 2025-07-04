cd2b0d41fac3dd490d184efed0d6c495df3a3fd0
jeissonperez <jeissonjoseperezquevedo@gmail.com>
2025-07-03 21:58:11 -0500

    actualizacion

--- src/paginas/Contacto.js
+++ src/paginas/Contacto.js
@@ -1,5 +1,21 @@
 const Contacto = () => {
-  return <h1>Contacto </h1>;
+  return (
+    <>
+      <div className="contactoFondo">
+        <section className="contacto">
+          <h2>Contacto</h2>
+          <p>Si tienes alguna pregunta o propuesta, no dudes en escribirme:</p>
+
+          <ul className="infoContacto">
+            <li> <img src="https://img.icons8.com/color/48/000000/gmail-new.png" alt="Gmail" /><strong>Correo:</strong> jeisson.dev@gmail.com</li>
+            <li> <img src="https://img.icons8.com/color/48/000000/whatsapp--v1.png" alt="WhatsApp" /><strong>WhatsApp:</strong> +57 300 123 4567</li>
+
+          </ul>
+        </section>
+      </div>
+    </>
+  )
+
 };
 
 
--- src/paginas/Quienes.js
+++ src/paginas/Quienes.js
@@ -8,7 +8,7 @@ const QuienesSomos = () => {
                         Somos un equipo apasionado por la tecnología, la programación y el aprendizaje constante. Este blog nace con el objetivo de compartir conocimientos, resolver dudas y crear una comunidad en torno al desarrollo de software.
                     </p>
                     <p>
-                        Aquí encontrarás artículos, recursos y experiencias reales sobre lenguajes como Python, JavaScript, HTML, CSS y mucho más. Creemos que el conocimiento debe ser accesible para todos, y trabajamos cada día para aportar valor a quienes comienzan o avanzan en el mundo del desarrollo.
+                        Aquí encontrarás artículos, recursos y experiencias reales sobre lenguajes como Python, JavaScript, HTML y mucho más. Creemos que el conocimiento debe ser accesible para todos, y trabajamos cada día para aportar valor a quienes comienzan o avanzan en el mundo del desarrollo.
                     </p>
                 </div>
             </div>
